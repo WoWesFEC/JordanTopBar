cc639f820638d878889b5f9b1c74eaa3be9f74fa
Jordan Worm <jordan.worm96@gmail.com>
2020-01-28 16:29:09 -0600

    Added normalize.css and started working on the logo componenet

--- App/Components/Logo.js
+++ App/Components/Logo.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Logo = () => {
   return (
-    <div>Logo Area</div>
+    <div id="logo">
+      <picture>
+        <img src="https://www.lowes.com/images/logos/2016_lowes_logo/lowes_logo_rgb/LLowesLogo2016_Vertical_RGB.png"/>
+      </picture>
+    </div>
   );
 }
 
