523f30156b9e5af4b8c88045cd48fbedabc3a1dc
Jordan Worm <jordan.worm96@gmail.com>
2020-02-01 14:39:01 -0600

    Working search functionality every time you add a letter

--- App/App.js
+++ App/App.js
@@ -3,18 +3,37 @@ import Logo from './Components/Logo';
 import Search from './Components/Search';
 import Userarea from './Components/Userarea';
 import Deadlinks from './Components/Deadlinks';
-
+import axios from 'axios';
 class App extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      search: ''
+      search: '',
+      data: ''
     }
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange (e) {
-    this.setState({search: `${e.target.value}`})
+    this.setState({search: e.target.value}, () => {
+      axios.post('/search', {search: this.state.search})
+      .then ((data) => {
+        this.setState({data: data.data})
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    })
+    console.log(this.state.data);
+  }
+
+  handleEnterPressed (e) {
+    let code = e.keyCode || e.which;
+    if(code === 13) { 
+      // needs to make a request to my database, 
+      //get back the info, 
+      //then send that info off to the other components
+    } 
   }
 
   render () {
