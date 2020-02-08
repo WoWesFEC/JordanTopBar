ab65e41c3cd8fbf7c19941bf89e71a3a0cc07dd6
Jordan Worm <jordan.worm96@gmail.com>
2020-02-08 14:34:46 -0600

    final version

--- App/Components/Search.js
+++ App/Components/Search.js
@@ -12,9 +12,7 @@ class Search extends React.Component {
   render () {
     return (
       <div className="jsearch">
-        {/* <picture>
           <img id="jsearchglass" src="./images/searchglass.png"></img>
-        </picture> */}
           <input
             type="text" 
             id="jsearch" 
@@ -22,7 +20,7 @@ class Search extends React.Component {
             onChange={this.props.handleChange}
             value={this.props.search}>
           </input>
-        <div className={this.props.search.length ? "jddvisible" : "jddinvisible"} searchData={this.props.searchData}>
+          <div className={this.props.search.length ? "jddvisible" : "jddinvisible"} searchData={this.props.searchData}>
           <Dropdown className={this.props.search.length ? "jddvisible" : "jddinvisible"} searchData={this.props.searchData}/>
         </div>
       </div>
